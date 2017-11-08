b9c6ecdc3425228a045584b1ed4af1ccef76ae74
Eve Lee <evelee@berkeley.edu>
2017-11-08 13:30:43 -0800

    missing name added

--- codes/assign.py
+++ codes/assign.py
@@ -5,7 +5,7 @@ names = np.array(["Clement Bonnerot", "Maria Charisi", "Andre da Silva Schneider
          "Shea Garrison-Kimmel", "Davide Gerosa", "Francois Hebert", \
          "Cameron Hummels", "Astrid Lamberts", "Eve Lee", \
          "Yiqiu Ma", "Christine Corbett Moran", "Hiroki Nagakura", \
-         "Robyn Sanderson", "Jono Squire", "Natalia Storch", "Stephen Taylor", \
+         "Robyn Sanderson", "Jono Squire", "Leo Stein", "Natalia Storch", "Stephen Taylor", \
          "Coral Wheeler", "Jess McIver", "Alex Urban", \
          "TJ Massinger", "Brittany Kamai", "Andrew Wade", \
          "Johannes Eichholz", "Michael Coughlin"])
@@ -15,7 +15,7 @@ dates = np.array(["Nov 09 17","Nov 16 17","Nov 30 17","Dec 07 17",\
                   "Feb 01 18", "Feb 08 18", "Feb 15 18", "Feb 22 18", \
                   "Mar 01 18", "Mar 08 18", "Mar 15 18", "Mar 22 18", "Mar 29 18", \
                   "Apr 05 18", "Apr 12 18", "Apr 19 18", "Apr 26 18", \
-                  "May 03 18", "May 10 18", "May 17 18", "May 24 18"])
+                  "May 03 18", "May 10 18", "May 17 18", "May 24 18", "May 31 18"])
 
 speaker = np.copy(names)
 food = np.copy(names)
